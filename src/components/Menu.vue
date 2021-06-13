69749ff9a74b6d2152edeefcae733af966ee3327
An P.Do <do.phuan@gmail.com>
2021-06-13 17:59:08 +0700

    Update menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="fixed top-0 left-0 w-full h-screen bg-red-400 ml-20"
+    class="fixed top-0 left-0 w-full h-screen bg-red-400 mt-20 md:ml-20 md:mt-0"
     v-show="isShowMenu"
   >
     Menu over here
