bdcb854425cc6ef63098c80cf468d7d085913fad
An P.Do <do.phuan@gmail.com>
2021-06-13 20:56:32 +0700

    Add animation and layout for home page

---
+++ src/components/Animations/Cursor.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    class="triangle"
+    viewBox="-30 -30 120 120"
+    width="100"
+    height="100"
+    style="transform: rotate(185.44deg)"
+  >
+    <path d="M 0, 60 L 30, 0 60,60 Z" class="triangle-path"></path>
+    <path
+      d="M 0, 65 L 30, 5 60,65 Z"
+      fill="none"
+      stroke="white"
+      stroke-width="1"
+      class="triangle-stroke"
+      style="transform: translateX(5.30224px) translateY(-5.30224px)"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class TriangleCursor extends Vue {}
+</script>
+
+<style scoped></style>
--- src/components/IntroductionPage.vue
+++ src/components/IntroductionPage.vue
@@ -3,7 +3,6 @@
     class="
       w-screen
       h-screen
-      bg-hero-md
       flex
       md:flex-row
       flex-column
@@ -12,11 +11,11 @@
       p-16
     "
   >
-    <div class="left-container">
+    <div class="left-container w-full md:w-2/4">
       <p>
         {{ $t("introduction.hello") }}
       </p>
-
+      <h1>Phu An Do</h1>
       <p>
         {{ $t("introduction.description") }}
       </p>
@@ -24,16 +23,41 @@
       <div>
         {{ $t("introduction.find_me") }}
       </div>
+      <div class="social-networks flex flex-column">
+        <a href="javascript:void(0)" class="mr-4">
+          <i class="fab fa-linkedin" />
+        </a>
+        <a href="javascript:void(0)" class="mr-4">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
     </div>
 
-    <div class="right-container"></div>
+    <div
+      class="
+        right-container
+        items-center
+        justify-center
+        hidden
+        md:flex
+        md:w-2/4
+      "
+    >
+      <span>Hình tui</span>
+    </div>
+    <Follower />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import Follower from "@/components/Animations/Followers.vue";
 
-@Component
+@Component({
+  components: {
+    Follower,
+  },
+})
 export default class IntroductionPage extends Vue {}
 </script>
 
