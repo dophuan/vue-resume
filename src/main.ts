5a31306406229bad2f2fb8fa30479e4fa271aaf8
An P.Do <do.phuan@gmail.com>
2021-06-15 22:00:47 +0700

    Reallocate project structure

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./common/store";
+import store from "./common/stores";
 import VueI18n from "vue-i18n";
 import mixins from "@/common/mixins";
-import { messages, defaultLocale } from "@/common/language";
+import { messages, defaultLocale } from "@/common/lang";
 import "@/assets/scss/style.scss";
 import "@fortawesome/fontawesome-free/js/all.js";
 import "@fortawesome/fontawesome-free/css/all.css";
