54ca676d397b44ab857e1caccb8c63040482d736
An P.Do <do.phuan@gmail.com>
2021-06-15 21:10:17 +0700

    Add mixins

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./common/store";
 import VueI18n from "vue-i18n";
+import mixins from "@/common/mixins";
 import { messages, defaultLocale } from "@/common/language";
 import "@/assets/scss/style.scss";
 import "@fortawesome/fontawesome-free/js/all.js";
@@ -10,6 +11,7 @@ import "@fortawesome/fontawesome-free/css/all.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueI18n);
+Vue.mixin(mixins);
 
 const i18n = new VueI18n({
   messages,
